67d6a74fd2ce1166a45f0176fa023b5d973855c4
strong_kim <sseyoung513@naver.com>
2017-08-15 22:28:00 +0900

    initial

--- app/src/main/java/blog/strong_kim/doitmission_01/MainActivity.java
+++ app/src/main/java/blog/strong_kim/doitmission_01/MainActivity.java
@@ -1,7 +1,8 @@
 package blog.strong_kim.doitmission_01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button;
     }
 }
